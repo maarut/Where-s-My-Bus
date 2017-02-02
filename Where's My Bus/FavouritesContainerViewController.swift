70c550999e7f399b830ff22ca308253dbc73e9d4
Maarut Chandegra <contact@maarut.net>
2017-02-02 15:29:38 +0000

    Remove comments

--- Where's My Bus/FavouritesContainerViewController.swift
+++ Where's My Bus/FavouritesContainerViewController.swift
@@ -14,8 +14,6 @@ class FavouritesContainerViewController: UIViewController
     var dataController: DataController!
     var stopPoint: StopPoint?
     
-    private var adBannerViewHidden = false
-    
     private weak var informationOverlay: UIView!
     private weak var informationText: UILabel!
     
@@ -72,14 +70,12 @@ extension FavouritesContainerViewController: GADBannerViewDelegate
 {
     func adViewDidReceiveAd(bannerView: GADBannerView!)
     {
-        adBannerViewHidden = false
         Dispatch.mainQueue.async { self.bottomConstraint.constant = 0 }
         Dispatch.mainQueue.after(30 * Int64(NSEC_PER_SEC)) { self.requestAd() }
     }
     
     func adView(bannerView: GADBannerView!, didFailToReceiveAdWithError error: GADRequestError!)
     {
-        adBannerViewHidden = true
         Dispatch.mainQueue.async { self.bottomConstraint.constant = -bannerView.frame.height }
         Dispatch.mainQueue.after(30 * Int64(NSEC_PER_SEC)) { self.requestAd() }
     }
